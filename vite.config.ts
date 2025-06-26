21b7eb2054277f3415769f9a98f85fa60898f088
a.developer <developer@developer.ru>
2025-06-26 22:33:19 +0300

    fix(vite.config): исправлены алиасы

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,22 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-      react({
-        babel: {
-          plugins: [
-            [
-              'babel-plugin-styled-components',
-              {
-                displayName: true,
-                fileName: false,
-              },
-            ],
-          ],
-        },
-      })
-  ],
-  resolve: {
-    alias: {
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@constants': path.resolve(__dirname, './src/constants'),
-      '@images': path.resolve(__dirname, './src/assets/images'),
-    },
-  },
+    plugins: [
+        react({
+            babel: {
+                plugins: [
+                    [
+                        'babel-plugin-styled-components',
+                        {
+                            displayName: true,
+                            fileName: false,
+                        },
+                    ],
+                ],
+            },
+        }),
+        tsconfigPaths(),
+    ],
 })
